e853cf81675ca2af916be16516f979aae9ee74a1
debabratamca <debu_mca07@hotmail.com>
2020-06-22 12:14:15 +0530

    Add project files.

---
+++ CalculatorUITest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorUITest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorUITest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3af01450-a5ff-40b0-a7e9-0add7199b975")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Appium Driver URL it works like a windows Service on your PC  
+        private const string appiumDriverURI = "http://127.0.0.1:4723";
+        //Application Key of your UWA   
+        //U can use any .Exe file as well for open a windows Application  
+        //private const string calApp = "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App";
+        private const string calApp = @"C:\Users\Dev\source\repos\JenkinsTestWindows\MyCalculator\bin\Debug\MyCalculator.exe";
+        protected static WindowsDriver<WindowsElement> calSession;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            if (calSession == null)
+            {
+                //DriverOptions driverOptions = new DriverOptions();
+                
+                DesiredCapabilities appCapabilities = new DesiredCapabilities();
+                appCapabilities.SetCapability("app", calApp);
+                appCapabilities.SetCapability("deviceName", "WindowsPC");
+                //Create a session to intract with Calculator windows application  
+                //calSession = new WindowsDriver<WindowsElement>(new Uri(appiumDriverURI), appCapabilities);
+                calSession = new WindowsDriver<WindowsElement>(new Uri(appiumDriverURI),appCapabilities);
+                //Automate Button and Get answer from Calculator  
+
+                //find by Name  
+                calSession.FindElement(By.Name("Nine")).Click();
+                calSession.FindElement(By.Name("One")).Click();
+                calSession.FindElement(By.Name("Two")).Click();
+                calSession.FindElement(By.Name("Three")).Click();
+                calSession.FindElement(By.Name("Multiply by")).Click();
+                //find by automation id  
+                calSession.FindElementByAccessibilityId("num9Button").Click();
+                calSession.FindElementByAccessibilityId("equalButton").Click();
+                //getting value from textbox  
+                string ExpectedValue = calSession.FindElementByAccessibilityId("CalculatorResults").Text;
+                string ExpectedValue1 = ExpectedValue.Replace("Display is ", "").Replace(",", "");
+
+                //Testcases  
+                Assert.AreEqual(82107, Convert.ToInt64(ExpectedValue1));
+            }
+        }
+    }
+}
